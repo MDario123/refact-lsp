5388260831a2e5f1e2f29e7a2cb5873c81405d62
MDario123 <manuel.dario.oliver@gmail.com>
2025-01-10 16:08:13 +0100

    fix(clippy): return match directly and remove useless clone I know it says to release the lock, but that does not seem possible, as the lock warranties exclusive access, if exclusive access for read is not needed we might change that to a RWLock, if no exclusive access is needed at all, we might as well remove the Mutex.

--- src/at_commands/at_search.rs
+++ src/at_commands/at_search.rs
@@ -76,17 +76,17 @@ pub async fn execute_at_search(
     let api_key = api_key.unwrap();
 
     let vec_db = gcx.read().await.vec_db.clone();
-    let r = match *vec_db.lock().await {
+    let vec_db = vec_db.lock().await;
+    match *vec_db {
         Some(ref db) => {
             let top_n_twice_as_big = top_n * 2;  // top_n will be cut at postprocessing stage, and we really care about top_n files, not pieces
             // TODO: this code sucks, release lock, don't hold anything during the search
             let search_result = db.vecdb_search(query.clone(), top_n_twice_as_big, vecdb_scope_filter_mb, &api_key).await?;
-            let results = search_result.results.clone();
-            return Ok(results2message(&results));
+            let results = search_result.results;
+            Ok(results2message(&results))
         }
         None => Err("VecDB is not active. Possible reasons: VecDB is turned off in settings, or perhaps a vectorization model is not available.".to_string())
-    };
-    r
+    }
 }
 
 #[async_trait]
